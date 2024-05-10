c0efccd984a4cf296ce4e022c8f67b1b9c29f77c
amitcjc <amitcjcpune@gmail.com>
2024-05-10 19:18:47 +0530

    by cjc

--- GitB163/src/com/A.java
+++ GitB163/src/com/A.java
@@ -2,6 +2,13 @@ package com;
 
 public class A 
 {
+	public void m1()
+	{
+		System.out.println("M1");
+	}
+	
+	
+	
 public static void main(String[] args) {
 	System.out.println("This is Git Demo");
 }
